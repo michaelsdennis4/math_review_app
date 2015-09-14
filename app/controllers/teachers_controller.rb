7386512f1a101b52f526c766c2c05ccd94f5b8ea
Michael Dennis <michaelsdennis4@gmail.com>
2015-09-14 11:34:39 -0400

    teacher edit

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -8,4 +8,18 @@ class TeachersController < ApplicationController
 		@teacher = Teacher.find(params[:id])
 	end
 
+	def edit
+		@teacher = Teacher.find(params[:id])
+	end
+
+	def update
+		teacher = Teacher.find(params[:id])
+		if (teacher.update({first_name: params[:first_name], last_name: params[:last_name], email: params[:email], is_active: params[:is_active]}))
+			redirect_to "/teachers/#{teacher.id}"
+		else
+			binding.pry
+			redirect_to "/teachers/#{teacher.id}/edit"
+		end
+	end
+
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -17,8 +17,7 @@ class Teacher < ActiveRecord::Base
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 	validates :email, presence: true, uniqueness: true
-	validates :is_active, presence: true
-
+	
 	has_secure_password
 
 	has_many :sessions
