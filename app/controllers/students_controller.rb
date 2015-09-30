3cbd437f6fca65f6bb2d1ad476e534d3d5b7265d
Michael Dennis <michaelsdennis4@gmail.com>
2015-09-29 22:14:53 -0400

    student create and update

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -24,5 +24,15 @@ class StudentsController < ApplicationController
 		end
 	end
 
+	def update
+		student = Student.find(params[:id])
+		if (student.update({first_name: params[:first_name], last_name: params[:last_name], email: params[:email], grad_year: params[:grad_year], identification: params[:identification], status: params[:status]}))
+			binding.pry
+			redirect_to "/students/#{student.id}"
+		else
+			redirect_to "/students/#{student.id}/edit"
+		end
+	end
+
 	
 end
