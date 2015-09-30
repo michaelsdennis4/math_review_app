d0767739587f47d444914b7a34eae27daf4b509f
Michael Dennis <michaelsdennis4@gmail.com>
2015-09-29 20:42:56 -0400

    student new and edit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,28 @@
 class StudentsController < ApplicationController
 
 	def index
-		@students = Student.all
+		@students = Student.all.order(:last_name, :first_name)
+	end
+
+	def new
 	end
 
 	def show
 		@student = Student.find(params[:id])
 	end
+
+	def edit
+		@student = Student.find(params[:id])
+	end
+
+	def create
+		student = Student.create
+		if (student.update({first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], grad_year: params[:grad_year], identification: params[:identification]}))
+			redirect_to "/students"
+		else
+			redirect_to "/students/new"
+		end
+	end
+
 	
 end
